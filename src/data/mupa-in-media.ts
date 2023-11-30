f28ccb7eb1c9b6da2d295598d63e918dc715b50d
Ignacio A. Molina <strocsdev@gmail.com>
2023-11-30 14:25:22 -0300

    add: project page with data of media

---
+++ src/data/mupa-in-media.ts
@@ -0,0 +1,74 @@
+export const MUPA_IN_MEDIA = [
+  {
+    href: 'https://arquls.userena.cl/2023/09/huellas-imborrables-el-legado-de-las-primeras-arquitectas-en-la-region/',
+    media: 'ULS',
+    text: 'Huellas imborrables. El legado de las primeras arquitectas en la región. – Escuela de Arquitectura – Universidad de La Serena'
+  },
+  {
+    href: 'https://www.ucentral.cl/noticias/la-serena/academica-dirige-proyecto-que-busca-relevar-el-legado-de-las-primeras',
+    media: 'UCEN',
+    text: 'Académica dirige proyecto que busca relevar el legado de las primeras arquitectas en la región'
+  },
+  {
+    media: 'TIERRAMARILLANO',
+    href: 'https://tierramarillano.cl/2023/08/28/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'HVARADIO',
+    href: 'https://www.hvaradio.cl/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'RADIOAMANCAY',
+    href: 'https://radioamancay.cl/2023/08/28/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'LA VOZ DEL NORTE ',
+    href: 'https://www.lavozdelnorte.cl/2023/08/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region-de-coquimbo/'
+  },
+  {
+    media: 'RADIO RUTA NORTE ',
+    href: 'https://radiorutanorte.cl/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/?fbclid=IwAR1ybYQBOlb3USVWANuhrdPB9sXMDqy41PjBOu0rMzjPbbZoTHBZnADKSH8'
+  },
+  {
+    media: 'VALLENAR DIGITAL',
+    href: 'https://portalweb.vallenardigital.cl/noticias-destacadas/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'RADIO ANIVERSARIO',
+    href: 'https://radioaniversario.cl/2023/08/28/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'DIARIO LA REGIÓN',
+    href: 'https://www.diariolaregion.cl/edicion-martes-29-de-agosto-2023'
+  }, {
+    media: 'EL SERENENSE',
+    href: 'https://www.elserenense.cl/regionales/2023/08/29/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region/'
+  },
+  {
+    media: 'MADERO',
+    href: 'https://www.madero.cl/documental-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region-de-coquimbo/'
+  },
+  {
+    media: 'ARICA365',
+    href: 'https://www.arica365.cl/blog/2023/08/29/proyecto-busca-relevar-el-legado-de-las-primeras-arquitectas-en-la-region'
+  },
+
+  {
+    media: 'FACEBOOK - LA MAÑANA INTERACTIVA',
+    href: 'https://www.facebook.com/radioestrelladelnorte/posts/pfbid0226hZbhtxfpw738ds7MDfUQwV1bRk7EjTkabhM2curQ7D8hWNt9Uo8KBW8KydbdMrl'
+  },
+  {
+    media: 'FACEBOOK - Limarí Televisión',
+    href: 'https://www.facebook.com/limaritv/posts/pfbid0A4LEBc4UKUpuaPxcmWZFcBL5BQ7qwjycmw4MeiFqhQyYwoiwRSzAgRGfAb3CGRAul'
+  },
+  {
+    media: 'FACEBOOK - CB NOTICIAS CHILE',
+    href: 'https://www.facebook.com/CBNoticiasChile/posts/pfbid02w7qc3fN1YGxK3dRc5QTMVXW8ai7SConr5xchGVrkNMjyLALTpjHhLfiem6ohg3y7l'
+  },
+  {
+    media: 'FACEBOOK - Red Privada de Emergencia Regional Noticias',
+    href: 'https://www.facebook.com/REDPRIVADADEEMERGENCIANOTICIAS/posts/pfbid0M3MY5mRsW1ro6Bmf5WSTwQbtqt1YktJraY9Q3XWX5RqEBKRzLw7qQF5137YodvnYl'
+  },
+]
+
+
