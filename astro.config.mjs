0327f6c0c06058cc3572e009b5c033f3e1fe2197
Ignacio A. Molina <strocsdev@gmail.com>
2023-11-29 22:41:39 -0300

    chore: add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,9 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mupa.cl',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  redirects: {
+    '/arquitectas-referentes': '/',
+    '/arquitectas-vitalicias': '/'
+  }
 })
