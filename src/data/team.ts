b04dfb9ac7f0f2bf4cbf0898b59ddad07b65cb26
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-01 16:19:18 -0300

    add[team]: data and basic template

---
+++ src/data/team.ts
@@ -0,0 +1,69 @@
+import oliviaPhoto from '../assets/team/Olivia Fox.jpg'
+import stephaniePhoto from '../assets/team/Stephanie Lara Alfaro.jpg'
+import camilaPhoto from '../assets/team/Camila Valencia.jpg'
+import caroPhoto from '../assets/team/Caro Novo Boza.jpg'
+import solangePhoto from '../assets/team/Sol Miranda Coletti.jpg'
+import lissetePhoto from '../assets/team/Lisette Gómez.jpeg'
+
+export const TEAM = [
+  {
+    name: 'Olivia Fox Pedraza',
+    role: 'Directora del proyecto',
+    description: 'Olivia es arquitecta, máster en asentamientos humanos y medio ambiente. Posee un diplomado en educación superior y es docente de diversas asignaturas, entre ellas arquitectura y urbanismo con perspectiva de género,donde ha podido relevar los roles de la mujer en la profesión y visibilizar las experiencias que las mujeres experimentan en el espacio público.',
+    image: oliviaPhoto
+  },
+  {
+    name: 'Stephanie Lara Alfaro',
+    role: 'Directora de Investigación',
+    description: 'Stephanie es arquitecta y docente, es candidata a doctora en arquitectura y urbanismo en la FAUD-UNSJ. Diplomada en Géneros, ciudades y territorios FAU-UNT. Sus temas de interés e investigación están orientados en la sustentabilidad social, la inclusión de género en el territorio y la difusión de aportes femeninos en la arquitectura y urbanismo.',
+    image: stephaniePhoto
+  },
+  {
+    name: 'Camila Valencia Altamirano',
+    role: 'Encargada de Difusión y Vinculación con el Medio',
+    description: 'Camila es arquitecta y gestora cultural. Diplomada en Patrimonio Cultural y Gestión Territorial. Integró la organización de la XIX Bienal de Arquitectura y Urbanismo de Chile en sus versiones 2015 y 2019. Ha dirigido múltiples proyectos de difusión de la producción arquitectónica desarrollada por estudiantes en universidades chilenas. Con interés en temáticas de vivienda, urbanismo, cultura y patrimonio.',
+    image: camilaPhoto,
+  },
+  {
+    name: 'Caro Novo Boza',
+    role: 'Directora Creativa',
+    description: 'Caro es arquitecta, máster en estudios avanzados de la arquitectura ETSAB-UPC, diplomada en apreciación estética y docencia en educación superior. Sus intereses y el desarrollo de sus proyectos abarcan el ámbito de la enseñanza, la estética, y la difusión de la disciplina a la comunidad bajo una perspectiva de género.',
+    image: caroPhoto,
+  },
+  {
+    name: 'Solange Miranda Coletti',
+    role: 'Encargada de producción',
+    description: 'Solange es arquitecta, máster en especialización y profundización de Arquitectura en Tierra y Patrimonio, DSA TERRE, Ensag, Francia. Diplomada en acabados finos de revestimientos en Tierra Terra Migaki Design, Italia. Diplomada en diseño de paisaje en la PUC Chile. Su experiencia está basada en la elaboración y ejecución de proyectos de restauración e investigación de patrimonio y territorio.',
+    image: solangePhoto,
+  },
+  {
+    name: 'Lisette Gómez Brizuela',
+    role: 'Comunicaciones',
+    description: 'Lisette es periodista y licenciada en comunicación social, graduada de la Universidad de La Serena, con una firme pasión por la comunicación. Su trayectoria académica incluye la obtención de diplomados en áreas clave de la comunicación: en Marketing Digital y en Comunicación en la Era Digital. ',
+    image: lissetePhoto,
+  },
+]
+
+
+export const COLLABORATES = [
+  {
+    name: 'Séptimo Rastro',
+    role: 'Audiovisual',
+  },
+  {
+    name: 'Ignacio Molina',
+    role: 'Desarollo Web',
+  },
+  {
+    name: 'Catalina Gálvez',
+    role: 'Estudiante colaboradora ULS',
+  },
+  {
+    name: 'Bryan Gómez',
+    role: 'Estudiante colaborador UCEN',
+  },
+  {
+    name: 'Leontina Loyola',
+    role: 'Estudiante colaborador UCEN',
+  },
+]
