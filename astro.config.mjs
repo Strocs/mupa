9f740c75ddd213f71bcc7112d1288bb7e9196008
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-14 20:52:20 -0300

    add: web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 
-import vercel from '@astrojs/vercel/static'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mupa.cl',
@@ -11,12 +9,5 @@ export default defineConfig({
   redirects: {
     '/arquitectas-referentes': '/',
     '/arquitectas-vitalicias': '/'
-  },
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-      imageService: true
-    }
-  })
+  }
 })
