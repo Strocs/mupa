bcb94cc23c4d6bdaffa77984041f606c27200566
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-14 20:31:30 -0300

    chore: ass vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
 import sitemap from '@astrojs/sitemap'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,11 @@ export default defineConfig({
   redirects: {
     '/arquitectas-referentes': '/',
     '/arquitectas-vitalicias': '/'
-  }
+  },
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 })
