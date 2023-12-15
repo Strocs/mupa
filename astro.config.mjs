844d9d882d1fcb9884032a68db407fb62eebb435
Ignacio A. Molina <strocsdev@gmail.com>
2023-12-14 21:06:58 -0300

    add: vercel on astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import sitemap from '@astrojs/sitemap'
 
+import vercel from '@astrojs/vercel/static'
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mupa.cl',
@@ -9,5 +11,9 @@ export default defineConfig({
   redirects: {
     '/arquitectas-referentes': '/',
     '/arquitectas-vitalicias': '/'
-  }
+  },
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  })
 })
